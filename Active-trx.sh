95691d1057e759e6e886bfefa1e766d6f0b1e2bf
RAYEES NOOR <120762806+MRxNOOR-119@users.noreply.github.com>
2023-05-09 13:42:21 +0430

    Add files via upload

---
+++ Active-trx.sh
@@ -0,0 +1,80 @@
+echo ""
+echo ""
+echo ""
+echo "The script is  made by noor tech"
+echo ""
+echo ""
+echo ""
+echo "Join My Telegram"
+echo ""
+echo ""
+echo ""
+echo "Installtion process is started" 
+echo ""
+echo ""
+echo ""
+
+pkg update -y                    
+pkg install python -y           
+pkg install python2 -y
+pkg install ruby -y 
+pkg install git -y
+pkg install screenfetch -y
+pkg install cowsay -y
+pkg install toilet -y
+pkg install figlet -y
+pkg install php -y 
+pkg install perl -y
+pkg install nmap -y
+pkg install openssh -y
+pkg install bash -y
+pkg install clang -y
+pkg install jq -y
+pkg install nano -y
+pkg install curl -y
+pkg install tar -y
+pkg install zip -y
+pkg install fish -y
+pkg install unzip -y
+pkg install tor -y 
+pkg install sudo -y
+pkg install wget -y
+pkg install tor -y
+pkg install w3m -y
+pkg install cmatrix -y
+pkg install wcalc -y 
+pkg install openssl -y
+pkg install bmon -y
+pkg upgrade -y
+apt update
+apt upgrade
+apt install git
+pip install bs4
+pip2 install bs4
+pip install requests
+pip2 install mechanize
+pip2 install requests
+pip install mechanize
+echo ""
+echo ""
+echo ""
+echo ""
+
+echo "All basic packages and tools is installed"
+os.system("xdg-open https://t.me/KingMazar")
+echo ""
+echo ""
+echo "
+         ███╗   ██╗ ██████╗  ██████╗ ██████╗            
+         ████╗  ██║██╔═══██╗██╔═══██╗██╔══██╗           
+         ██╔██╗ ██║██║   ██║██║   ██║██████╔╝           
+         ██║╚██╗██║██║   ██║██║   ██║██╔══██╗           
+         ██║ ╚████║╚██████╔╝╚██████╔╝██║  ██║           
+         ╚═╝  ╚═══╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝              
+                                                                
+echo " Telegram Channel  : https://t.me/NOOR_TECH_538
+youtube chanel : BLACK TEAM
+github ; https://github.com/MRXNOOR-119"
+echo ""
+echo " this script craeted by NOOR TECH👌😌❤🙋‍♂️   Subscribe our Youtube channel and press Bell icon"
+exit
